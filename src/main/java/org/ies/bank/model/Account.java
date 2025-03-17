f5c081ab3c3727c9f46747c179227a7890f2ee38
Jesus <jesus@b25>
2025-03-17 10:06:47 +0100

    TErminado

--- src/main/java/org/ies/bank/model/Account.java
+++ src/main/java/org/ies/bank/model/Account.java
@@ -12,5 +12,16 @@ public class Account {
     protected String nif;
     protected Double balance;
 
+    public boolean retired(Double amount){
+        if(amount > balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void deposit(double amount) {
+        balance += amount;
+    }
 
 }
--- src/main/java/org/ies/bank/model/Bank.java
+++ src/main/java/org/ies/bank/model/Bank.java
@@ -3,8 +3,7 @@ package org.ies.bank.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +11,85 @@ public class Bank {
     protected String name;
     protected Map<String, Account> accountsByIban;
     protected List<Customer> customers;
+
+    public void depositMoney(String iban, double amount) {
+        if (accountsByIban.containsKey(iban)) {
+            Account account = accountsByIban.get(iban);
+            account.setBalance((account.balance + amount));
+        } else {
+            System.out.println("No existe la cuenta");
+        }
+    }
+
+    public List<Account> findCustomerAccounts(String nif) {
+        if (customers.equals(nif)) {
+            List<Account> customerAccounts = new LinkedList<>();
+            for (Account account : accountsByIban.values()) {
+                if (account.getNif().equals(nif)) {
+                    customerAccounts.add(account);
+                }
+            }
+            return customerAccounts;
+        } else {
+            return null;
+        }
+    }
+
+    public Customer findCustomer(String nif) {
+        for (var customer : customers) {
+            if (customer.getNif().equals(nif)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public void retiredMoney(String iban, double amount) {
+        if (accountsByIban.containsKey(iban)) {
+            Account account = accountsByIban.get(iban);
+            account.retired(amount);
+        } else {
+            System.out.println("No existe la cuenta");
+        }
+    }
+
+    public void transfer(String ibanOrigin, String ibanDestination, double amount) {
+        if (accountsByIban.containsKey(ibanOrigin)) {
+            var accountOrigin = accountsByIban.get(ibanOrigin);
+            if (accountsByIban.containsKey(ibanDestination)) {
+                var accountDestination = accountsByIban.get(ibanDestination);
+                if (accountOrigin.retired(amount)) {
+                    accountDestination.deposit(amount);
+                } else {
+                    System.out.println("No hay suficiente dinero");
+                }
+            } else {
+                System.out.println("No existe la cuenta de origen");
+            }
+        } else {
+            System.out.println("No existe la cuenta origen");
+        }
+    }
+    public Set<String> showCustomer(int zipCode){
+    Set<String> nifs = new HashSet<>();
+   for (Customer customer: customers){
+       if (customer.getZipCode() == zipCode){
+           nifs.add(customer.getNif());
+       }
+   }
+   return nifs;
+    }
+    public List<Account> foundAccounts (int zipCode){
+        var nifs = showCustomer(zipCode);
+        List<Account>  accountList = new LinkedList<>();
+        for (Account account: accountsByIban.values()){
+            if (nifs.contains(account.getNif())){
+                accountList.add(account);
+            }
+        }
+        return accountList;
+    }
 }
+
+
+
